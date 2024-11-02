0776be6c0d5e3c405a68bcf0fe4aa024ee1cb045
neophyteCode44 <157976564+neophyteCode44@users.noreply.github.com>
2024-11-02 06:32:22 +0530

    Initial commit

---
+++ src/main/java/com/booktiva/servicebookingsystem/BooktivaApplication.java
@@ -0,0 +1,13 @@
+package com.booktiva.servicebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktivaApplication.class, args);
+	}
+
+}
